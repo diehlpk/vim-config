e74c9a4d616742406d844e9e9b7e469b70667014
Patrick Diehl <me@diehlpk.de>
2023-04-25 08:54:16 -0700

    Add install script for plugins

---
+++ install.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.vim/pack/plugins/start
+rm -rf ~/.vim/pack/plugins/start/vim-lastplace
+git clone --depth=1 https://github.com/farmergreg/vim-lastplace.git ~/.vim/pack/plugins/start/vim-lastplace
